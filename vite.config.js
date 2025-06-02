e32274283d15dd1a671492974a56fb04ecb312b2
maha-bel1 <balghouthimaha@gmail.com>
2025-06-02 17:49:36 +0200

    Update configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/innolink-plateforme-Demo/',
+  base: '/Innolink-demo/',
   plugins: [react()],
   // Environment variables configuration
   envPrefix: 'VITE_',
